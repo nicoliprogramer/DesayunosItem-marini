e617dd0f203e2a57d43fbfafb3205d7f59c40cf5
nicoliprogramer <imotivacionesf@gmail.com>
2022-09-07 23:47:00 -0300

    item

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import ItemListContainer from "./componentes/ItemListContainer";
+import NavBar from "./componentes/NavBar";
+
+
+function App() {
+  return (
+    <div>
+
+      <NavBar />
+      <ItemListContainer />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/NavBar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import logo from "./img/logo.png";
+
+
+const NavBar = () => {
+  return (
+    <div className="row">
+      <div className="col-md-12 bg-black text-white p-3">
+        <ul className="nav d-flex align-items-center">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#"><img src={logo} width="100px" /></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disablede link_header">WhatsApp</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link link_header" href="#">Inicio</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link link_header" href="#">Productos</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link link_header" href="#">Nosotros</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link  link_header" href="#">contacto</a>
+          </li>
+        </ul>
+
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
